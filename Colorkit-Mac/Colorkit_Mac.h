a54f2d7ed3c4001956b5cf1d211668677fa5dc31
Ben Scheirman <ben@scheirman.com>
2018-09-04 16:36:52 -0500

    Initial Commit

---
+++ Colorkit-Mac/Colorkit_Mac.h
@@ -0,0 +1,19 @@
+//
+//  Colorkit_Mac.h
+//  Colorkit-Mac
+//
+//  Created by Ben Scheirman on 9/4/18.
+//  Copyright © 2018 ficklebits. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Colorkit_Mac.
+FOUNDATION_EXPORT double Colorkit_MacVersionNumber;
+
+//! Project version string for Colorkit_Mac.
+FOUNDATION_EXPORT const unsigned char Colorkit_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Colorkit_Mac/PublicHeader.h>
+
+
